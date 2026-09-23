134ec4738e69b3bf4f5fc1f924f3cc1c4b8e1a73
Pragya Dixit <dixitpragya.2726@gmail.com>
2026-09-23 22:01:27 +0530

    Time: 0 ms (100%), Space: 52.2 MB (82.95%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int mx =0;
+        for(int i=0;i<nums.size();i++){
+            if(i>mx){
+                return false;
+            }
+            if(mx>=nums.size()-1){
+                return true;
+            }
+            mx = max(mx,i+nums[i]);
+        }
+        return true;
+    }
+};
